b26b3c6c0fd92435ed28866a86ce441ba35fa3f3
Chuck Kainrath <chuckkainrath@gmailcom>
2021-01-04 17:37:28 -0600

    completed phase 5

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -9,7 +9,27 @@ console.log("goodbye cruel world")
 let setCookie = (name, value) =>{
   document.cookie = `${name}=${value}`;
 }
+
 setCookie("monster_name ", "cookie")
 setCookie("favorite_cookie ", "snickerdoodle")
 
-window.alert(document.cookie);
+// window.alert(document.cookie);
+
+let getCookie = () => {
+  return document.cookie.split('; ');
+}
+
+let getCookieValue = name => {
+  let cookies = getCookie();
+  let result = cookies.filter(el => {
+    return (el.includes(name + '='));
+  });
+  if (result === []) {
+    return null;
+  } else {
+    let cookie = result[0];
+    return cookie.slice(cookie.indexOf('=') + 1);
+  }
+}
+
+window.alert(getCookieValue('monster_name'));
