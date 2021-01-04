e8d88dd56ed87a073ac1d0eef3a1342d73e6dc83
mike <mikemihalchik@gmail.com>
2021-01-04 18:21:09 -0500

    phase 4 complete

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -6,9 +6,10 @@ window.onload = () =>{
   console.log("hello world")
 }
 console.log("goodbye cruel world")
-
-
-document.cookie = "monster_name=cookie";
-document.cookie = "favorite_cookie=snickerdoodle";
+let setCookie = (name, value) =>{
+  document.cookie = `${name}=${value}`;
+}
+setCookie("monster_name ", "cookie")
+setCookie("favorite_cookie ", "snickerdoodle")
 
 window.alert(document.cookie);
